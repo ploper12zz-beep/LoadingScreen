97a6ab2ba76616bb9ad9e982a144b540db07e337
ploper12zz-beep <ploper12zz@gmail.com>
2026-09-23 14:03:35 +0700

    adding checkpoint every 3 file

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@
 for (int i = 1; i <= totalsteps; i++)
 {
     Console.WriteLine($"Loading.. file {i} of {totalsteps} completed.");
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("Checkpoint reached! Saving progress...\n");
+    }
 }
 
 Console.WriteLine("\nLoading complete!");
